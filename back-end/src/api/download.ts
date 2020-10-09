3af6ae0561156a4b381defb7b495ef16f5f28b28
Tkendi <acg70159@gmail.com>
2020-10-10 01:52:06 +0900

    ytdl filter code adding

--- back-end/src/api/download.ts
+++ back-end/src/api/download.ts
@@ -1,6 +1,7 @@
 import Router from 'koa-router';
 import ytdl from 'ytdl-core';
 import fs from 'fs';
+import { format } from 'path';
 
 const download = new Router();
 
@@ -8,7 +9,9 @@ download.get('/mp3/:context', async (ctx: any) => {
   try {
     const context = ctx.params;
     let title = 'audio';
-    await ytdl(`${context}`).pipe(fs.createWriteStream('video.flv'));
+    await ytdl(`${context}`, {
+      filter: (format : any) => format.container === 'mp3',
+    }).pipe(fs.createWriteStream('video.flv'));
     console.log('download success');
   } catch (e) {
     console.log(e);
