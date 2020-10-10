c91f6153fe73946fb48399df64648c3490adabae
Tkendi <acg70159@gmail.com>
2020-10-10 23:25:31 +0900

    download mp4 success

--- back-end/src/api/download.ts
+++ back-end/src/api/download.ts
@@ -4,18 +4,22 @@ import fs from 'fs';
 
 const download = new Router();
 
-download.get('/:context', async (ctx: any) => {
+download.get('/', async (ctx: any) => {
   try {
-    let URL = ctx.params;
+    let URL = 'http://www.youtube.com/watch?v=A02s8omM_hI';
+    const success = 'success';
 
-    ctx.header('Content-Disposition', 'attachment; filename="video.mp3"');
+    ctx.set('Content-Disposition', 'attachment; filename="video.mp3"');
 
     ytdl(URL, {
       filter: (format) => format.container === 'mp4',
     }).pipe(fs.createWriteStream('video.flv'));
 
-    console.log('Download Success');
-  } catch (e) {
+    console.log(success);
+
+    ctx.body = 'Download Success';
+    return success;
+  } catch (e: any) {
     console.log(e);
   }
 });
