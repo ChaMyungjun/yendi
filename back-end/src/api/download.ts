add3fd9348efdd949d82051f1c71f351aa42720e
Tkendi <acg70159@gmail.com>
2020-10-11 01:28:04 +0900

    mp3 download code adding

--- back-end/src/api/download.ts
+++ back-end/src/api/download.ts
@@ -4,6 +4,8 @@ import fs from 'fs';
 
 const download = new Router();
 
+//query getting
+
 download.get('/mp4', async (ctx: any) => {
   try {
     let { URL } = ctx.request.query;
@@ -13,15 +15,40 @@ download.get('/mp4', async (ctx: any) => {
       console.log(URL);
       const success = 'success';
 
-      ctx.set('Content-Disposition', 'attachment; filename="video.mp3"');
+      ctx.set('Content-Disposition', 'attachment; filename="video.mp4"');
 
       ytdl(URL, {
-        filter: (format) => format.container === 'mp4',
-      }).pipe(fs.createWriteStream('video.flv'));
+        filter: (format) => format.container === 'ts',
+      }).pipe(fs.createWriteStream('video.mp4'));
 
       console.log(success);
 
-      ctx.body = 'Download Success';
+      ctx.body = 'Download Success Video';
+      return success;
+    }
+  } catch (e: any) {
+    console.log(e);
+  }
+});
+
+download.get('/mp3', async (ctx: any) => {
+  try {
+    let { URL } = ctx.request.query;
+    if (!URL) {
+      return null;
+    } else {
+      console.log(URL);
+      const success = 'success';
+
+      ctx.set('Context-Disposition', 'attachment; filename="video.mp3"');
+
+      ytdl(URL, {
+        filter: (format) => format.container === 'mp4',
+      }).pipe(fs.createWriteStream('audio.mp3'));
+
+      console.log(success);
+
+      ctx.body = 'Download Success Audio';
       return success;
     }
   } catch (e: any) {
