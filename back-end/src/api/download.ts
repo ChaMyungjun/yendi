6925977c308e78b8b8fc7775dcffa65179e8cae8
Tkendi <acg70159@gmail.com>
2020-10-10 18:24:05 +0900

    download server change code

--- back-end/src/api/download.ts
+++ back-end/src/api/download.ts
@@ -1,18 +1,18 @@
 import Router from 'koa-router';
 import ytdl from 'ytdl-core';
 import fs from 'fs';
-import { format } from 'path';
 
 const download = new Router();
 
 download.get('/mp3/:context', async (ctx: any) => {
   try {
-    const context = ctx.params;
-    let title = 'audio';
-    await ytdl(`${context}`, {
-      filter: (format : any) => format.container === 'mp3',
-    }).pipe(fs.createWriteStream('video.flv'));
-    console.log('download success');
+    let URL = ctx.params;
+
+    ctx.header('Content-Disposition', 'attachment; filename="video.mp3"');
+
+    ytdl(URL, {
+      format: 'mp3'
+    }).pipe(ctx);
   } catch (e) {
     console.log(e);
   }
