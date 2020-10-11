38897bc1b61ba37d53320c09082f0659a534e21e
Tkendi <acg70159@gmail.com>
2020-10-11 18:17:11 +0900

    mobx action function settting

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,8 +1,10 @@
 import { observable, action, toJS } from "mobx";
 import * as searched from "../lib/api/searching";
+import * as downloaded from "../lib/api/download";
 
 export default class youtube {
   @observable searching: Object = [];
+  @observable downloading: Object = [];
 
   @action.bound
   find = async (search_t: any) => {
@@ -11,6 +13,17 @@ export default class youtube {
     } else {
       this.searching = await searched.search(search_t);
       (<any>this.searching) = toJS(this.searching);
+    }
+  };
+
+  @action.bound
+  download = async (title: any) => {
+    const success = "Downloading Success";
+    if (!title) {
+      return null;
+    } else {
+      this.downloading = await downloaded.mp3(title);
+      (<any>this.downloading) = success;
     }
   };
 }
