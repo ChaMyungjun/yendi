e4f78498026bc7cd3e1a9a8931f98ad821bc8fb6
Tkendi <acg70159@gmail.com>
2020-10-14 20:16:42 +0900

    authTemplate initialize

---
+++ front-end/src/components/auth/authTemplate.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+class authTemplate extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <div>
+            <Typography>authTemplate</Typography>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default authTemplate;
