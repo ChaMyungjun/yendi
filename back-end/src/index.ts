4a05c9c8ce78117caf28ff898a292c403ad4efbb
Tkendi <acg70159@gmail.com>
2020-10-17 14:55:51 +0900

    mongodb connection

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,19 +1,36 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-import bodyParser from 'koa-bodyparser'
-import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import mongoose from 'mongoose';
 import api from './api';
+
+const { PORT, MONGO_URI } = process.env;
 
 const app = new Koa();
 const router = new Router();
 
-router.use('/api', api.routes())
+mongoose
+  .connect('mongodb://localhost:27017/yendi', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connect to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
-app.use(cors())
-app.use(bodyParser())
+//Router setting
+router.use('/api', api.routes());
+
+app.use(cors());
+app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log('Listening to port 4000');
 });
